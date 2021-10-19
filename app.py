1612fb1e6ac5bf640c7e948dd3ea6f45e949d006
Arthur <arthur.mendonca@estudante.firjan.senai.br>
2021-10-18 23:45:41 -0300

    Correção de conflitos e adições de imports

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from src.classes.pokemon import Pokemon
 from src.dao.pokemon_dao import PokemonDAO
+from src.bd.bd_connection import bd
 
-novo= Pokemon(1, "teste", "teste", "124","teste")
+implementa_descricao= "Greninja é a forma evoluída de Frogadier e a forma final de Froakie, Greninja é um mestre de rapidez e furtividade."
+novo= Pokemon(1, "Greninja", "Água e Sombrio", "Frokie, Frogadier e Greninja ",implementa_descricao)
 nova_dao = PokemonDAO(novo)
-print(nova_dao)
+bd.insert_in_BD(nova_dao._programacao_json)
--- src/dao/pokemon_dao.py
+++ src/dao/pokemon_dao.py
@@ -7,8 +7,8 @@ class PokemonDAO():
         self._programacao_json['id'] = instancia.id
         self._programacao_json['Nome'] = instancia.nome
         self._programacao_json['Tipo'] = instancia.tipo
-        self._programacao_json['Evoluções'] = instancia.evolucoes
-        self._programacao_json['Descrição'] = instancia.descricao
+        self._programacao_json['Evolucoes'] = instancia.evolucoes
+        self._programacao_json['Descricao'] = instancia.descricao
   
     def __str__(self):
         return str(self._programacao_json)
